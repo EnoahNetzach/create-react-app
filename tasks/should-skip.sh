0bcdd0789ca4d5f71c79fb07807066b5e7b5b94d
Enoah Netzach <f.castellarin@gmail.com>
2017-03-05 17:35:36 +0100

    WIP

--- tasks/should-skip.sh
+++ tasks/should-skip.sh
@@ -1,13 +1,10 @@
 #!/usr/bin/env bash
 
 baseBranch=${TRAVIS_BRANCH}
-currentBranch=${TRAVIS_PULL_REQUEST_BRANCH}
+currentBranch=${TRAVIS_PULL_REQUEST_SHA}
 
 # If not in a Pull Request, then never skip
 if [ "$currentBranch" = "" ]; then echo 1; exit; fi
 
-# Get the name list of changed files
-diff=`git diff --name-only "$baseBranch".."$currentBranch"`
-
-# Count non-".md" files have been changed
-echo diff | sed -E '/\s*.+\.md\s*/d' | wc -l
+# Skip if only ".md" files have been changed
+git diff --name-only "$baseBranch".."$currentBranch" | sed -E '/\s*.+\.md\s*/d' | wc -l
