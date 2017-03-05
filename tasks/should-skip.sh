85a56c64befad72e48e1e968d3be986d219c3895
Enoah Netzach <f.castellarin@gmail.com>
2017-03-05 21:06:32 +0100

    WIP appveyor

--- tasks/should-skip.sh
+++ tasks/should-skip.sh
@@ -1,13 +1,10 @@
 #!/usr/bin/env bash
 
-
-node -e "$nodeProgram"
-
 if [ "$TRAVIS" = "true" ]; then
   commitRange=${TRAVIS_COMMIT_RANGE}
 fi
 
-if [ "$APPVEYOR" = "true" ]; then
+if [ "$APPVEYOR" = "true" ] && [ "$APPVEYOR_PULL_REQUEST_NUMBER" != "" ]; then
   prData=`curl -s https://api.github.com/repos/${APPVEYOR_REPO_NAME}/pulls/${APPVEYOR_PULL_REQUEST_NUMBER}`
   nodeProgram="var prData = $prData;if (prData.base && prData.head) console.log(prData.base.sha + '..' + prData.head.sha);"
   echo ${nodeProgram}
